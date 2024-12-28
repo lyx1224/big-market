636b0075812807e34a591006f4a7a1abd8d07f3a
lyx <1325329314@qq.com>
2024-12-28 21:07:26 +0800

    feat:实现了积分入库与积分流水订单的事务操作，并更新了签到返利的接口，通过switch返利的类型来发送不同的MQ消息。

--- big-market-trigger/src/main/java/cn/lyx/trigger/listener/RebateMessageCustomer.java
+++ big-market-trigger/src/main/java/cn/lyx/trigger/listener/RebateMessageCustomer.java
@@ -3,9 +3,15 @@ package cn.lyx.trigger.listener;
 
 import cn.lyx.domain.activity.model.entity.SkuRechargeEntity;
 import cn.lyx.domain.activity.service.IRaffleActivityAccountQuotaService;
+import cn.lyx.domain.credit.model.entity.TradeEntity;
+import cn.lyx.domain.credit.model.valobj.TradeNameVO;
+import cn.lyx.domain.credit.model.valobj.TradeTypeVO;
+import cn.lyx.domain.credit.service.ICreditAdjustService;
 import cn.lyx.domain.rebate.event.SendRebateMessageEvent;
 import cn.lyx.domain.rebate.model.valobj.RebateTypeVO;
+import cn.lyx.types.enums.ResponseCode;
 import cn.lyx.types.event.BaseEvent;
+import cn.lyx.types.exception.AppException;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.TypeReference;
 import lombok.extern.slf4j.Slf4j;
@@ -15,6 +21,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+
+import java.math.BigDecimal;
 
 import static java.lang.Character.getType;
 
@@ -32,30 +40,47 @@ public class RebateMessageCustomer {
 
     @Resource
     private IRaffleActivityAccountQuotaService raffleActivityAccountQuotaService;
+    @Resource
+    private ICreditAdjustService creditAdjustService;
+
     @RabbitListener(queuesToDeclare = @Queue(value = "${spring.rabbitmq.topic.send_rebate}"))
     public void listener(String message){
         try{
-            log.error("监听用户行为返利消息 topic：{} message：{}",topic,message);
+            log.info("监听用户行为返利消息 topic：{} message：{}",topic,message);
             // 1. 转换消息
             BaseEvent.EventMessage<SendRebateMessageEvent.RebateMessage> eventMessage = JSON.parseObject(message, new TypeReference<BaseEvent.EventMessage<SendRebateMessageEvent.RebateMessage>>() {
             }.getType());
             SendRebateMessageEvent.RebateMessage rebateMessage = eventMessage.getData();
-            if(!RebateTypeVO.SKU.getCode().equals(rebateMessage.getRebateType())){
-                log.info("监听用户行为返利消息 - 非SKU奖励暂时不处理 topic:{} message:{}", topic,message );
-                return;
-            }
 
             //2.入账奖励
-            SkuRechargeEntity skuRechargeEntity = new SkuRechargeEntity();
-            skuRechargeEntity.setUserId(rebateMessage.getUserId());
-            skuRechargeEntity.setSku(Long.valueOf(rebateMessage.getRebateConfig()));
-            skuRechargeEntity.setOutBusinessNo(rebateMessage.getBizId());
-            raffleActivityAccountQuotaService.createOrder(skuRechargeEntity);
-            log.error("监听用户行为返利消息，成功！ topic：{} message：{}",topic,message);
-
-        }catch (Exception e){
-            log.error("监听用户行为返利消息，消费失败 topic：{} message：{}",topic,message,e);
+            switch (rebateMessage.getRebateType()) {
+                case "sku":
+                    SkuRechargeEntity skuRechargeEntity = new SkuRechargeEntity();
+                    skuRechargeEntity.setUserId(rebateMessage.getUserId());
+                    skuRechargeEntity.setSku(Long.valueOf(rebateMessage.getRebateConfig()));
+                    skuRechargeEntity.setOutBusinessNo(rebateMessage.getBizId());
+                    raffleActivityAccountQuotaService.createOrder(skuRechargeEntity);
+                    break;
+                case "integral":
+                    TradeEntity tradeEntity = new TradeEntity();
+                    tradeEntity.setUserId(rebateMessage.getUserId());
+                    tradeEntity.setTradeName(TradeNameVO.REBATE);
+                    tradeEntity.setTradeType(TradeTypeVO.FORWARD);
+                    tradeEntity.setAmount(new BigDecimal(rebateMessage.getRebateConfig()));
+                    tradeEntity.setOutBusinessNo(rebateMessage.getBizId());
+                    creditAdjustService.createOrder(tradeEntity);
+                    break;
+            }
+        } catch (AppException e) {
+            if (ResponseCode.INDEX_DUP.getCode().equals(e.getCode())) {
+                log.warn("监听用户行为返利消息，消费重复 topic: {} message: {}", topic, message, e);
+                return;
+            }
+            throw e;
+        } catch (Exception e) {
+            log.error("监听用户行为返利消息，消费失败 topic: {} message: {}", topic, message, e);
             throw e;
         }
     }
+
 }
