aa219748ce07e6208c6cb89b796da566a1fc5dd4
tianlei <15136725+t1anlei@user.noreply.gitee.com>
2025-01-01 23:35:10 +0800

    feat:dubbo的配置开发，修改处为yml文件中宿主机地址换成自己的局域网地址192.那个

--- big-market-api/src/main/java/cn/lyx/trigger/api/dto/RaffleAwardListRequestDTO.java
+++ big-market-api/src/main/java/cn/lyx/trigger/api/dto/RaffleAwardListRequestDTO.java
@@ -2,13 +2,15 @@ package cn.lyx.trigger.api.dto;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 /**
  * @author lyx
  * @description 抽奖奖品列表，请求对象
  * @since 2024/11/17
  */
 @Data
-public class RaffleAwardListRequestDTO {
+public class RaffleAwardListRequestDTO implements Serializable {
     // 用户ID
     private String userId;
     // 抽奖活动ID
--- big-market-api/src/main/java/cn/lyx/trigger/api/dto/RaffleStrategyRequestDTO.java
+++ big-market-api/src/main/java/cn/lyx/trigger/api/dto/RaffleStrategyRequestDTO.java
@@ -2,13 +2,15 @@ package cn.lyx.trigger.api.dto;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 /**
  * @author lyx
  * @description 抽奖请求参数
  * @since 2024/11/17
  */
 @Data
-public class RaffleStrategyRequestDTO {
+public class RaffleStrategyRequestDTO implements Serializable {
     // 抽奖策略ID
     private Long strategyId;
 }
